a93ee5382c38030f6aac6b17e167f0c3f2760f64
Piotr Kopalko <copalco@gmail.com>
2020-03-13 18:38:53 +0100

    Add event manager emit method

--- tests/test_events_manager.py
+++ tests/test_events_manager.py
@@ -2,10 +2,12 @@ import unittest
 
 
 class EventManager:
-    pass
+    
+    def emit(self):
+        pass
 
 
 class TestEventManager(unittest.TestCase):
 
     def test_manager_can_emit_event(self) -> None:
-        EventManager
+        EventManager().emit()
