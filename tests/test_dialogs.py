108c3550f287202f412ffd32563469a80c0afd7a
Piotr Kopalko <copalco@gmail.com>
2020-03-13 22:48:03 +0100

    Use line generator in dialog tests

--- tests/test_dialogs.py
+++ tests/test_dialogs.py
@@ -11,12 +11,12 @@ class TestDialog(unittest.TestCase):
     def test_dialog_can_have_npc_lines(self) -> None:
         dialog = Dialog()
         dialog.add_npc_line("I'm Npc. What's your name?")
-        assert dialog.lines == [NpcLine("I'm Npc. What's your name?")]
+        assert next(dialog.line()) == NpcLine("I'm Npc. What's your name?")
 
     def test_dialog_can_have_player_lines(self) -> None:
         dialog = Dialog()
         dialog.add_player_line("I'm the player.")
-        assert dialog.lines == [PlayerLine("I'm the player.")]
+        assert next(dialog.line()) == PlayerLine("I'm the player.")
 
     def test_dialog_are_added_to_initial_stage(self) -> None:
         dialog = Dialog()
