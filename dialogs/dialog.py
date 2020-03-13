33fe643f58219f0e8c42c35a3d4c290e3126e54f
Piotr Kopalko <copalco@gmail.com>
2020-03-13 21:41:36 +0100

    Make dialog add npc lines as class instead of str

--- dialogs/dialog.py
+++ dialogs/dialog.py
@@ -1,10 +1,13 @@
+from dialogs.line import NpcLine
+
+
 class Dialog:
 
     def __init__(self) -> None:
         self.lines = []
 
     def add_npc_line(self, text: str) -> None:
-        self.lines.append(text)
+        self.lines.append(NpcLine(text))
 
     def add_player_line(self, text: str) -> None:
         self.lines.append(text)
--- tests/test_dialogs.py
+++ tests/test_dialogs.py
@@ -1,6 +1,7 @@
 import unittest
 
 from dialogs.dialog import Dialog
+from dialogs.line import NpcLine
 
 
 class TestDialog(unittest.TestCase):
@@ -8,7 +9,7 @@ class TestDialog(unittest.TestCase):
     def test_dialog_can_have_npc_lines(self) -> None:
         dialog = Dialog()
         dialog.add_npc_line("I'm Npc. What's your name?")
-        assert dialog.lines == ["I'm Npc. What's your name?"]
+        assert dialog.lines == [NpcLine("I'm Npc. What's your name?")]
 
     def test_dialog_can_have_player_lines(self) -> None:
         dialog = Dialog()
