7db7cbf192131735b7a6941638b5ab76663086ae
Piotr Kopalko <copalco@gmail.com>
2020-03-13 19:11:14 +0100

    Move event manager to events package

--- events/events_manager.py
+++ events/events_manager.py
@@ -2,3 +2,21 @@ import typing
 
 
 Subscriber = typing.Callable[['Event'], None]
+
+
+class EventManager:
+
+    def __init__(self) -> None:
+        self.subscribers: typing.List[Subscriber] = []
+        self.events = []
+
+    def emit(self, event: 'Event') -> None:
+        self.events.append(event)
+        for subscriber in self.subscribers:
+            subscriber(event)
+
+    def subscribe(self, callback: Subscriber) -> None:
+        self.subscribers.append(callback)
+
+    def history(self) -> typing.List['Event']:
+        return self.events[::-1]
--- tests/test_events_manager.py
+++ tests/test_events_manager.py
@@ -1,25 +1,7 @@
 import unittest
 
 from events.event import Event
-from events.events_manager import Subscriber
-
-
-class EventManager:
-
-    def __init__(self) -> None:
-        self.subscribers: typing.List[Subscriber] = []
-        self.events = []
-
-    def emit(self, event: 'Event') -> None:
-        self.events.append(event)
-        for subscriber in self.subscribers:
-            subscriber(event)
-
-    def subscribe(self, callback: Subscriber) -> None:
-        self.subscribers.append(callback)
-
-    def history(self) -> typing.List['Event']:
-        return self.events[::-1]
+from events.events_manager import EventManager
 
 
 class TestEventManager(unittest.TestCase):
