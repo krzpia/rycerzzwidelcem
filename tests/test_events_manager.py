643549638d4bcc1b706deaaff6080ae2bc67a460
Piotr Kopalko <copalco@gmail.com>
2020-03-13 18:47:20 +0100

    Store emitted event in events manager

--- tests/test_events_manager.py
+++ tests/test_events_manager.py
@@ -3,15 +3,22 @@ import unittest
 
 class EventManager:
 
+    def __init__(self) -> None:
+        self.events = []
+
     def emit(self, event: 'Event') -> None:
-        pass
+        self.events.append(event)
 
 
 class Event:
-    pass
+
+    def __eq__(self, other: 'Event') -> bool:
+        return True
 
 
 class TestEventManager(unittest.TestCase):
 
     def test_manager_can_emit_event(self) -> None:
-        EventManager().emit(Event())
+        manager = EventManager()
+        manager.emit(Event())
+        assert manager.events == [Event()]
