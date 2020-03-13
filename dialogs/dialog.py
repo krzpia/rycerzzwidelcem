8aed82748af8373bc4e9c121cd0c7726556b87ed
Piotr Kopalko <copalco@gmail.com>
2020-03-13 22:44:40 +0100

    Remove lines attribute from dialog

--- dialogs/dialog.py
+++ dialogs/dialog.py
@@ -11,17 +11,14 @@ class Dialog:
     def __init__(self) -> None:
         self.stage = 0
         self.setup_stage = 0
-        self.lines: typing.List[Line] = []
         self.lines_by_stage: typing.Dict[int, typing.List[Line]] = {
             self.setup_stage: []
         }
 
     def add_npc_line(self, text: str) -> None:
-        self.lines.append(NpcLine(text))
         self.lines_by_stage[self.setup_stage].append(NpcLine(text))
 
     def add_player_line(self, text: str) -> None:
-        self.lines.append(PlayerLine(text))
         self.lines_by_stage[self.setup_stage].append(PlayerLine(text))
 
     def lines_for_stage(self, stage: int) -> typing.List[Line]:
