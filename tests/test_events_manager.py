3640753aaeb1a398f3b5704bee4bead045ba2bd0
Piotr Kopalko <copalco@gmail.com>
2020-03-13 18:41:28 +0100

    Accept event while emitting

--- tests/test_events_manager.py
+++ tests/test_events_manager.py
@@ -2,12 +2,16 @@ import unittest
 
 
 class EventManager:
-    
-    def emit(self):
+
+    def emit(self, event: 'Event') -> None:
         pass
+
+
+class Event:
+    pass
 
 
 class TestEventManager(unittest.TestCase):
 
     def test_manager_can_emit_event(self) -> None:
-        EventManager().emit()
+        EventManager().emit(Event())
